452a7b0aef6988cfa1bf3b0550130cabf69d9209
Bogdan Buzac <bogdanbuzac@gmail.com>
2013-12-10 17:30:35 +0200

    m to inches conversion formula

--- src/demo/bean/ConverterBean.java
+++ src/demo/bean/ConverterBean.java
@@ -1,8 +1,8 @@
 // --------------------------------------------------------------------------
-// Copyright © 2012, SPM Software Inc. All Rights Reserved.
-// This program belongs to SPM Software Inc.  It is considered a TRADE SECRET and
-// is not to be divulged or used by parties who have not received written
-// authorization from SPM Software Inc.
+// Copyright © 2012 - 2013 Optymyze Pte. Ltd. All Rights Reserved.
+// This program belongs to Optymyze Pte. Ltd. It is considered a TRADE SECRET
+// and is not to be divulged or used by parties who have not received written
+// authorization from Optymyze Pte. Ltd.
 // --------------------------------------------------------------------------
 package demo.bean;
 
