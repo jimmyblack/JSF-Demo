70616693d921d7e724797f30a7bf0deec0441028
Bogdan Buzac <bogdanbuzac@gmail.com>
2013-12-10 16:34:12 +0200

    first commit

---
+++ src/demo/bean/ConverterBean.java
@@ -0,0 +1,24 @@
+// --------------------------------------------------------------------------
+// Copyright © 2012, SPM Software Inc. All Rights Reserved.
+// This program belongs to SPM Software Inc.  It is considered a TRADE SECRET and
+// is not to be divulged or used by parties who have not received written
+// authorization from SPM Software Inc.
+// --------------------------------------------------------------------------
+package demo.bean;
+
+/**
+ * @author Bogdan Buzac
+ */
+public class ConverterBean {
+
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+}
